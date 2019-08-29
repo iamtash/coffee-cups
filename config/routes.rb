65a79610d78aaabd0a5a6d859919c15085a65070
iamtash <natashachernis@gmail.com>
2019-08-28 22:27:39 -0500

    draw routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'roasters/new'
-  get 'roasters/index'
-  get 'roasters/show'
-  get 'users/new'
-  get 'users/show'
-  get 'cups/new'
-  get 'cups/edit'
-  get 'cups/show'
-  get 'cups/index'
-  get 'coffees/new'
-  get 'coffees/edit'
-  get 'coffees/show'
-  get 'coffees/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :roasters do
+    resources :coffees
+  end
+
+  resources :users, only: [:new, :create, :show] do
+    resources :cups, only: [:new, :create, :edit, :show]
+  end
+
+  get 'cups/index' => 'cups#index'
+  root 'cups#index'
+  
 end
