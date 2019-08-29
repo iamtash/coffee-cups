13eb4c99c7b0411f171af550e74fb322f6dbb801
iamtash <natashachernis@gmail.com>
2019-08-28 22:06:37 -0500

    wire up associations

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,2 +1,5 @@
 class Coffee < ApplicationRecord
+    belongs_to :roaster
+    has_many :cups
+    has_many :users, through: :cups
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :cups
+    has_many :coffees, through: :cups
 end
