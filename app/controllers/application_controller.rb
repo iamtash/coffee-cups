0fd423e2404c620ae7bdd4e2862a85738f5cd9b5
iamtash <natashachernis@gmail.com>
2019-08-29 21:35:02 -0500

    write login action/status helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    def login(email:, password:)
+        user = User.find_by(email: email)
+        if user && user.authenticate(password)
+          session[:user_id] = user.id
+        else
+          session[:message] = "Login failed. Please enter your email and password to log in."
+          redirect_to login_path
+        end
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  
+    def logged_in?
+        !!current_user
+    end
+
 end
+
