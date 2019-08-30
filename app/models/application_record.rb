3d953e32b20a3e260571191aa607102c488b3d0e
iamtash <natashachernis@gmail.com>
2019-08-29 21:37:55 -0500

    write abstract name normalizing method for all models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def normalize(attributes)
+    attributes.each {|k, v| self.send ("#{k}="), v.downcase.capitalize if v}
+  end
 end
