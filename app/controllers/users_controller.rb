5e97c5edd55c8db7050eb0f5ccaf636a5db05e77
iamtash <natashachernis@gmail.com>
2019-08-29 21:36:36 -0500

    user signup and show controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(email: @user.email, password: @user.password)
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    redirect_to login_path unless logged_in?
+    @user = User.find_by(id: params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
+
+
+
+
