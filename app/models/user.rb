6e00c58f1507bbbf8f659ae66757ef060d91d286
iamtash <natashachernis@gmail.com>
2019-08-29 21:38:37 -0500

    normalize user first and last name before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
     has_many :cups
     has_many :coffees, through: :cups
     has_secure_password
+    before_validation :normalize_full_name
     validates :first_name, :last_name, :password, presence: true
     validates :email, uniqueness: true
+   
+    def normalize_full_name
+        normalize(first_name: first_name)
+        normalize(last_name: last_name)
+    end
 end
