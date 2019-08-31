ec6100cd56e838362ecb1c0b1c601a3c3976f8e5
iamtash <natashachernis@gmail.com>
2019-08-31 12:17:22 -0500

    cup functionality

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -2,6 +2,13 @@ class Coffee < ApplicationRecord
     belongs_to :roaster
     has_many :cups
     has_many :users, through: :cups
-    validates :name, uniqueness: {scope: :roaster}
+    validates :name, presence: true, uniqueness: {scope: :roaster}
     validates :roast, :roaster, presence: true
+
+    ROASTS = %w(Light Medium Dark)
+
+    def self.roasts 
+        ROASTS
+    end
+
 end
