3b951a56bd2ca409ac42bc63d0f0c0f5a315a79a
iamtash <natashachernis@gmail.com>
2019-08-31 12:16:57 -0500

    cup functionality

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -1,5 +1,17 @@
 class CupsController < ApplicationController
+  before_action :require_login
+
   def new
+    @cup = Cup.new
+    @brews = Cup.brews
+    @coffees = Coffee.all
+    @roasts = Coffee.roasts
+  end
+
+  def create
+    raise params.inspect
+    @cup = cup.new(cup_params)
+    @cup.user = current_user
   end
 
   def edit
@@ -10,4 +22,15 @@ class CupsController < ApplicationController
 
   def index
   end
+
+  private
+    def cup_params
+      params.require(:cup).permit(:brew, :coffee_id, coffee_attributes: [
+        :name,
+        :roast,
+        :roaster_id
+      ]
+      )
+    end
+
 end
--- app/models/cup.rb
+++ app/models/cup.rb
@@ -2,4 +2,33 @@ class Cup < ApplicationRecord
     belongs_to :user
     belongs_to :coffee
     validates :user, :coffee, :brew, presence: true
+
+    BREWS = %w(
+        Drip
+        Iced
+        Cold brew
+        Pourover
+        Espresso
+        Americano
+        Macchiato
+        Cortado
+        Flat white
+        Cappuccino
+        Latte
+        Aeropress
+        Siphon
+        French press
+        Moka pot
+        Turkish
+        Chemex
+    )
+
+    def self.brews 
+        BREWS
+    end
+
+    def coffee_attributes=(coffee_attributes)
+        self.coffee = Coffee.find_or_create_by(name: coffee[:name])
+        self.coffee.update(coffee)
+    end
 end
