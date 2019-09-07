af74e72a4cb3087acb2f7a886bce18a07f71b5f7
iamtash <natashachernis@gmail.com>
2019-09-06 19:31:46 -0500

    tweak validations/BREWS, define nested attributes

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,34 +1,44 @@
 class Cup < ApplicationRecord
     belongs_to :user
     belongs_to :coffee
-    validates :user, :coffee, :brew, presence: true
+    validates :user, :brew, presence: true
+    validates_associated :coffee
 
-    BREWS = %w(
-        Drip
-        Iced
-        Cold brew
-        Pourover
-        Espresso
-        Americano
-        Macchiato
-        Cortado
-        Flat white
-        Cappuccino
-        Latte
-        Aeropress
-        Siphon
-        French press
-        Moka pot
-        Turkish
-        Chemex
-    )
+    BREWS = [
+        'Drip',
+        'Iced',
+        'Cold brew',
+        'Pourover',
+        'Espresso',
+        'Americano',
+        'Macchiato',
+        'Cortado',
+        'Flat white',
+        'Cappuccino',
+        'Latte',
+        'Aeropress',
+        'Siphon',
+        'French press',
+        'Moka pot',
+        'Turkish',
+        'Chemex',
+    ]
 
     def self.brews 
         BREWS
     end
 
     def coffee_attributes=(coffee_attributes)
-        self.coffee = Coffee.find_or_create_by(name: coffee[:name])
-        self.coffee.update(coffee)
+        self.coffee = Coffee.where(name: coffee_attributes[:name]).first_or_create do |coffee|
+            coffee.update(coffee_attributes)
+        end
+    end
+
+    def coffee_rating
+        self.coffee.rating.rating
+    end
+
+    def sip_date
+        self.created_at.strftime("%A, %B %-d, %Y")
     end
 end
