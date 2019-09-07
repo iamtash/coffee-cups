354a650a2a1ed621eaaabfb1c33b1fd4d611050f
iamtash <natashachernis@gmail.com>
2019-09-07 18:55:15 -0500

    write controller actions for user login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    
+    redirect_to root_path if logged_in?
+    @user = User.new
   end
 
   def create
-
+    login(email: user_params[:email], password: user_params[:password])
   end
 
   def destroy
-
+    session.delete :user_id
+    redirect_to login_path
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
