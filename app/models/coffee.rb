53aefa8664987f2dc0091aec0c609512d4a33857
iamtash <natashachernis@gmail.com>
2019-09-06 19:30:56 -0500

    define nested attributes, add validations

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -2,13 +2,21 @@ class Coffee < ApplicationRecord
     belongs_to :roaster
     has_many :cups
     has_many :users, through: :cups
+    has_many :ratings
     validates :name, presence: true, uniqueness: {scope: :roaster}
-    validates :roast, :roaster, presence: true
+    validates :roast, presence: true
+    validates_associated :roaster
 
     ROASTS = %w(Light Medium Dark)
 
     def self.roasts 
         ROASTS
+    end
+
+    def roaster_attributes=(roaster_attributes)
+        self.roaster = Roaster.where(name: roaster_attributes[:name]).first_or_create do |roaster|
+            roaster.update(roaster_attributes)
+        end
     end
 
 end
