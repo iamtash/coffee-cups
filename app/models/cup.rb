f72f30ba7c1e77c0e879e020d125cc14b107ca7a
github username <github email address>
2019-09-10 12:18:03 -0600

    remove brew PORO logic from cup model, define rating nested attributes writer

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,38 +1,19 @@
 class Cup < ApplicationRecord
     belongs_to :user
     belongs_to :coffee
-    before_validation :normalize_brew
-    validates :user, :brew, presence: true
+    belongs_to :brew
+    has_one :rating
+    validates :user, presence: true
     validates_associated :coffee
-
-    BREWS = [
-        'Drip',
-        'Iced',
-        'Cold brew',
-        'Pourover',
-        'Espresso',
-        'Americano',
-        'Macchiato',
-        'Cortado',
-        'Flat white',
-        'Cappuccino',
-        'Latte',
-        'Aeropress',
-        'Siphon',
-        'French press',
-        'Moka pot',
-        'Turkish',
-        'Chemex',
-    ]
-
-    def self.brews 
-        BREWS
-    end
 
     def coffee_attributes=(coffee_attributes)
         self.coffee = Coffee.where(name: coffee_attributes[:name]).first_or_create do |coffee|
             coffee.update(coffee_attributes)
         end
+    end
+
+    def rating_attributes=(rating_attributes)
+        self.build_rating(rating_attributes)
     end
 
     def coffee_rating
@@ -46,13 +27,4 @@ class Cup < ApplicationRecord
     def sip_date
         self.created_at.strftime("%A, %B %-d, %Y")
     end
-
-    def a_or_an
-        brew.start_with?(/[aeiou]/) ? 'an' : 'a'
-    end
-    
-    private
-        def normalize_brew
-            self.brew = brew.split(' ').map {|w| w.downcase}.join(' ')
-        end
 end
