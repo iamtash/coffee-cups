7a4c6d9c51b9a465df04365b3222bbdef014bc4a
github username <github email address>
2019-09-11 14:33:54 -0600

    modify user model to allow EITHER secure login with pw OR github OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
     has_many :coffees, through: :cups
     has_secure_password
     before_validation :normalize_full_name
-    validates :first_name, :last_name, :password, presence: true
+    validates :first_name, presence: true
+    validates :last_name, :password, presence: true, unless: :github_nickname
     validates :email, presence: true, uniqueness: true
-    validates :password, confirmation: { case_sensitive: true }
+    validates :password, confirmation: { case_sensitive: true }, unless: :github_nickname
    
     def normalize_full_name
         normalize(first_name: first_name)
