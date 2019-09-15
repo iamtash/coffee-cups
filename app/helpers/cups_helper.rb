1bdb88004de16c3513d616f61299c40e51048bde
iamtash <natashachernis@gmail.com>
2019-09-15 18:17:04 -0500

    refactor with helpers

--- app/helpers/cups_helper.rb
+++ app/helpers/cups_helper.rb
@@ -6,4 +6,8 @@ module CupsHelper
     def display_cup_rating(cup)
         "#{cup.user.first_name} rated this cup #{cup.rating.out_of_five}." if cup.rating
     end
+
+    def cup_url(cup)
+        user_cup_path(cup.user, cup)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def youve_or_other(current_user)
+        current_user == @user ? "you've" : "#{@user.first_name} has"
+    end
+
+    def who_hasnt(current_user)
+        current_user == @user? "You haven't" : "#{@user.first_name} hasn't"
+    end
+
+    def user_link(user)
+        link_to(user.first_name, user)
+    end
 end
