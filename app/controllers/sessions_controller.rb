97505b4d86460fd75643f8c7db73db82054d2a5e
iamtash <natashachernis@gmail.com>
2019-09-15 18:31:45 -0500

    move OAuth logic from SessionsController to User model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,27 +28,11 @@ class SessionsController < ApplicationController
       request.env['omniauth.auth']
     end
 
-    def parse_name(user)
-      first_last = auth['info']['name'].split(' ')
-      if first_last.length == 1
-        user.first_name = first_last 
-      else 
-        user.first_name = first_last.first
-        user.last_name = first_last.last
-      end
-    end
-
     def oauth_login(auth)
-      user = User.find_or_initialize_by(github_nickname: auth['info']['nickname']) do |u|
-          parse_name(u)
-          u.email = auth['info']['email'] || auth['info']['nickname']
-          u.image = auth['info']['image']
-          u.password = 'chocolatecoveredalmonds'
-          u.password_confirmation = 'chocolatecoveredalmonds'
-      end
+      user = User.from_oauth(auth)
       user.save
       session[:user_id] = user.id
       redirect_to root_path
-  end
+    end
 end
 
