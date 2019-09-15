8283e8ebb34c03abe3b904e7e082fdffa4fca887
iamtash <natashachernis@gmail.com>
2019-09-15 18:30:49 -0500

    move OAuth logic from SessionsController to User model, build simple recommendations feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,61 @@
 class User < ApplicationRecord
     has_many :cups
     has_many :coffees, through: :cups
+    has_many :ratings, through: :cups
     has_secure_password
-    before_validation :normalize_full_name
+    before_validation :normalize_full_name, on: :create
     validates :first_name, presence: true
     validates :last_name, :password, presence: true, unless: :github_nickname
     validates :email, presence: true, uniqueness: true
     validates :password, confirmation: { case_sensitive: true }, unless: :github_nickname
    
     def normalize_full_name
-        normalize(first_name: first_name)
-        normalize(last_name: last_name)
+        normalize(first_name: first_name, last_name: last_name)
     end
+
+    def self.from_oauth(auth)
+        User.find_or_initialize_by(github_nickname: auth['info']['nickname']) do |u|
+            parse_github_name(u)
+            u.email = auth['info']['email'] || auth['info']['nickname']
+            u.image = auth['info']['image']
+            u.password = 'chocolatecoveredalmonds'
+            u.password_confirmation = 'chocolatecoveredalmonds'
+        end
+    end
+
+    def parse_github_name(user)
+        first_last = auth['info']['name'].split(' ')
+        if first_last.length == 1
+          user.first_name = first_last 
+        else 
+          user.first_name = first_last.first
+          user.last_name = first_last.last
+        end
+    end
+
+    def favorite_coffees
+        # ratings.select {|rating| [4, 5].include?(rating.rating)}
+        self.ratings.joins(:user).where(rating: [4,5]).collect {|rating| rating.coffee}
+    end
+
+    def self.compare_favorite_coffees(current_user) # returns users with similar taste in coffee to the current user
+        self.select {
+            |u| current_user.favorite_coffees.any? {
+                |coffee| u.favorite_coffees.include?(coffee)
+                }
+            }.reject {|u| u == current_user}
+    end
+
+    def coffee_recommendations(similar_users)
+        similar_users.collect {|similar_user| similar_user.favorite_coffee
+                    }.reject {|coffee| self.coffees.include?(coffee)}    
+    end
+
+    def make_recommendations(current_user)
+        if self == current_user
+            self.coffee_recommendations(User.compare_favorite_coffees(self))
+        end
+    end
+
 end
+
