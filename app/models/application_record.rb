99ecd5096656e8b29aa090dae081a4a1b0a7c8d1
iamtash <natashachernis@gmail.com>
2019-09-15 18:18:12 -0500

    write alphabetizing scope method inherited by any class with name attribute

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def normalize(attributes)
-    attributes.each {|k, v| self.send ("#{k}="), v.downcase.capitalize if v}
+    attributes.each {|k, v| self.send ("#{k}="), v.downcase.titleize if v}
   end
+
+  scope :ordered_by_name, -> { order("name asc") }
 end
