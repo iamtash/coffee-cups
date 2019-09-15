2227631c5ee6e29e27d62a86d94cb36b2475c47d
iamtash <natashachernis@gmail.com>
2019-09-15 18:31:24 -0500

    normalize user input before persisting to roasters table

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,4 +1,11 @@
 class Roaster < ApplicationRecord
     has_many :coffees
+    before_validation :normalize_name_and_location
     validates :name, :location, presence: true
+
+    private
+        def normalize_name_and_location
+            normalize(name: name, location: location)
+        end
 end
+
