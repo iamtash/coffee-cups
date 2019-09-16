47f7ba278c5693b328014fec1b67241d6b5c0c3e
iamtash <natashachernis@gmail.com>
2019-09-15 20:40:48 -0500

    minor style edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,26 +35,24 @@ class User < ApplicationRecord
 
     def favorite_coffees
         # ratings.select {|rating| [4, 5].include?(rating.rating)}
-        self.ratings.joins(:user).where(rating: [4,5]).collect {|rating| rating.coffee}
+        self.ratings.joins(:user).where(rating: [4,5]).map {|rating| rating.coffee}
     end
 
-    def self.compare_favorite_coffees(current_user) # returns users with similar taste in coffee to the current user
+    def self.compare_favorite_coffees(user) # returns users with similar taste in coffee to the current user
         self.select {
-            |u| current_user.favorite_coffees.any? {
+            |u| user.favorite_coffees.any? {
                 |coffee| u.favorite_coffees.include?(coffee)
-                }
-            }.reject {|u| u == current_user}
+            }
+        }.reject {|u| u == current_user}
     end
 
     def coffee_recommendations(similar_users) # returns a collection of coffees recommended for the user to try
         similar_users.collect {|similar_user| similar_user.favorite_coffee
-                    }.reject {|coffee| self.coffees.include?(coffee)}    
+        }.reject {|coffee| self.coffees.include?(coffee)}    
     end
 
     def make_recommendations(current_user) #runner method for users#show
-        if self == current_user
-            self.coffee_recommendations(User.compare_favorite_coffees(self))
-        end
+        self.coffee_recommendations(User.compare_favorite_coffees(self)) if self == current_user
     end
 
 end
